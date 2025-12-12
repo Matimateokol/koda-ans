2e25ca82f07c74bc1455e02defb55383dc04b6da
Natchuz <self@natchuz.com>
2025-12-12 01:04:25 +0100

    Added decoding

--- koder_rans.py
+++ koder_rans.py
@@ -22,11 +22,48 @@ def rans_encode(data: bytes, model: rANSData) -> bytes:
     out.reverse()
     return bytes(out)
 
+
+def rans_decode(data: bytes, model: rANSData):
+    out = bytearray()
+    N = model.interval_size
+    R = model.renormalization_size
+
+    # Last bytes were written as LE, but were later reversed, thus we can read them as BE
+    state = int.from_bytes(data[0:4], 'big')
+    mask = (2 ** N) - 1
+    cursor = 4
+
+    # Find s such that cdf(s) < x_i+1 & mask < cdf(s+1)
+    def get_symbol():
+        for s_i in range(0, len(model.distributor) - 1):
+            if model.distributor[s_i] <= (state & mask) < model.distributor[s_i + 1]:
+                return s_i
+        else:
+            return len(model.distributor) - 1
+
+    END_OF_INTERVAL = 1 << (R + 1)
+
+    while cursor < len(data):
+        while state < END_OF_INTERVAL:
+            state <<= N
+            state |= data[cursor]
+            cursor += 1
+        s = get_symbol()
+        out.append(s)
+        state = model.frequency[s] * (state >> N) + (state & mask) - model.distributor[s]
+
+    return bytes(out)
+
+
 if __name__ == "__main__":
-    # 
+
     ransdata = rANSData([0, 2, 3], [2, 1, 1], 2, 3)
     data = b'\x01\x00'
-    rans_encode(data,ransdata)
+
+    encoded = rans_encode(data, ransdata)
+    decoded = rans_decode(encoded, ransdata)
+    assert data == decoded
+
     directory = Path.joinpath(Path.cwd(), "rozklady_testowe")
     # if directory.is_dir():
     #     with open(path, "rb") as f:
